e34d1d413e55298a6fffce2466bd7dc2e9d127aa
junaidnasir361 <junaidnasir361@8009bfac-b52d-3d0c-4d26-0a977b782919>
2013-09-26 19:22:01 +0000

    Modified: simple1d.m, comments and magnetic component plot Added: medium1d.m with a slab of medium having e=2*e0 Remarks: algorithm goes unstable after passing through the slab, however if magnetic component's imp is kept constant i.e 377 it shows some reflection

--- trunk/Matlab/simple1d.m
+++ trunk/Matlab/simple1d.m
@@ -1,21 +1,33 @@
 SIZE=201;
 ez=zeros(1,SIZE);
 hy=zeros(1,SIZE);
-imp0=377;
+imp0=377; %squareroot(u0/e0)
 maxTime = 1001;
 mm=0;
 temp=0;
-for qTime = 1:(maxTime-1)
-    for  mm = 1:(SIZE - 2)
-        hy(mm) = hy(mm) + (ez(mm + 1) - ez(mm)) / imp0;
+    for qTime = 1:(maxTime-1)
+%        Update Magnetic field
+        for  mm = 1:(SIZE - 2)
+            hy(mm) = hy(mm) + (ez(mm + 1) - ez(mm)) / imp0;
+        end
+%         Update Electrical filed
+        for mm = 2:(SIZE-1)
+            ez(mm) = ez(mm) + (hy(mm) - hy(mm - 1)) * imp0;
+        end
+%         Source node (hard coded)
+        ez(1) = exp(-(qTime - 30) * (qTime - 30) / 100.);
+%         Plotting
+        figure(1);
+        grid on; 
+        subplot(2,1,1);
+        plot(1:SIZE,ez);
+        title('Electirc Component');
+        Xlim([0 SIZE]);
+        ylim([-1.2 1.2]);
+        subplot(2,1,2);
+        plot(1:SIZE,hy);
+        title('Magnetic Component');
+        Xlim([0 SIZE]);
+        ylim([-0.003 0.003]);
+        pause(0.02);
     end
-    for mm = 2:(SIZE-1)
-        ez(mm) = ez(mm) + (hy(mm) - hy(mm - 1)) * imp0;
-    end
-    ez(1) = exp(-(qTime - 30) * (qTime - 30) / 100.);
-    figure(1);
-    grid on; 
-    ylim([-1.2 1.2])
-    plot(1:201,ez);
-    pause(0.02);
-end
