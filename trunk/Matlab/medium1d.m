123c352c66cd960cabf9a35f856c8fea44c55ce3
junaidnasir361 <junaidnasir361@8009bfac-b52d-3d0c-4d26-0a977b782919>
2013-09-28 07:03:01 +0000

    Fixed: boundary value of imp for magnetic component

--- trunk/Matlab/medium1d.m
+++ trunk/Matlab/medium1d.m
@@ -3,7 +3,7 @@ ez=zeros(1,SIZE);
 hy=zeros(1,SIZE);
 % introducing a medium having e=2*e0 (slab of length 20) -->
 % imp=squareroot(u0/2*e0)=265.77
-imp=[377*ones(1,SIZE-80) 266*ones(1,20) 377*ones(1,60)];
+imp=[377*ones(1,SIZE-79) 266*ones(1,20) 377*ones(1,59)];
 % imp0=377; %squareroot(u0/e0) = 377
 maxTime = 1001;
 mm=0;
@@ -11,13 +11,11 @@ temp=0;
     for qTime = 1:(maxTime-1)
 %        Update Magnetic field
         for  mm = 1:(SIZE - 2)
-            hy(mm) = hy(mm) + (ez(mm + 1) - ez(mm)) / imp(1); %stable
-%             with some refelction of waves
-%             hy(mm) = hy(mm) + (ez(mm + 1) - ez(mm)) / imp(mm); %unstable
+            hy(mm) = hy(mm) + (ez(mm + 1) - ez(mm)) / ((imp(mm)+imp(mm+1))/2);
         end
 %         Update Electrical filed
         for mm = 2:(SIZE-1)
-            ez(mm) = ez(mm) + (hy(mm) - hy(mm - 1)) * imp(mm);
+            ez(mm) = ez(mm) + (hy(mm) - hy(mm - 1)) * imp(mm) ;
         end
 %         Source node (hard coded)
         ez(1) = exp(-(qTime - 30) * (qTime - 30) / 100.);
@@ -28,17 +26,15 @@ temp=0;
         plot(1:SIZE,ez);
         title('Electirc Component');
         Xlim([0 SIZE]);
-%         ylim([-1.2 1.2]);
-        z=ylim;
-        line([SIZE-80 SIZE-80],[z(1) z(2)],'Color','Red')
-        line([SIZE-60 SIZE-60],[z(1) z(2)],'Color','Red')
+        ylim([-1.5 1.5]);
+        line([SIZE-80 SIZE-80],[-1.5 1.5],'Color','Red')
+        line([SIZE-60 SIZE-60],[-1.5 1.5],'Color','Red')
         subplot(2,1,2);
         plot(1:SIZE,hy);
         title('Magnetic Component');
         Xlim([0 SIZE]);
-%         ylim([-0.003 0.003]);
-        z=ylim;
-        line([SIZE-80 SIZE-80],[z(1) z(2)],'Color','Red')
-        line([SIZE-60 SIZE-60],[z(1) z(2)],'Color','Red')
+        ylim([-0.005 0.005]);
+        line([SIZE-80 SIZE-80],[-0.005 0.005],'Color','Red')
+        line([SIZE-60 SIZE-60],[-0.005 0.005],'Color','Red')
         pause(0.02);
     end
