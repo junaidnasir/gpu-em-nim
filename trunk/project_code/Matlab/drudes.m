1c1036413e4231183edea74a442aa2f7e85f10a5
junaidnasir361@gmail.com <junaidnasir361@gmail.com@8009bfac-b52d-3d0c-4d26-0a977b782919>
2014-06-01 15:44:58 +0000

    drudes.m updates
    observations
    1) Gaussian pulse is still out of shape
    2) wave inside the slab is distorted even after settling time
    3) input sin wave have a magnitude of (0,40) while output wave have (-20,20). it is shifted towards origin
    4) on increasing size of domain (i.e 1024 -> 2048) input wave magnitude also doubles (0,80)

--- trunk/project_code/Matlab/drudes.m
+++ trunk/project_code/Matlab/drudes.m
@@ -1,9 +1,11 @@
+%% Variables and stuff
+clear;
 clc;
 a=13;
-SIZE=1024;   
-maxTime =20*1024;
+SIZE=1048;   
+maxTime =5*SIZE;
 
-SourceSelect=1; % 0=Sinosoidal, 1=Gauassian
+SourceSelect=0; % 0=Sinosoidal, 1=Gauassian
 % if (SourceSelect==0)
 %     maxTime = 5001;
 % end
@@ -27,7 +29,12 @@ mur=-1;
 mu_nought=1.2566e-006;
 epsilon_nought=8.8542e-012;
 % Drudes model variables
-
+mu_inf=1;
+    epsilon_inf=1;
+    omega_pe2= 4*pi*pi*f*f*(epsilon_inf-epsilonr);%Plasma frequency electric squared
+    omega_pm2= 4*pi*pi*f*f*(mu_inf-mur);%Plasma frequency magnetic squared
+    ro_m=0;
+    ro_e=0;
 % Incident and Refelected Waves Variables
 Eincident=0;
 Etransmitted=0;
@@ -35,46 +42,43 @@ Etemp=zeros(1,maxTime);
 
 
 % refractive index variables
-Z1 = (SIZE/2)+250;
+Z1 = (SIZE/2)+100;
 z1 = Z1*delx;
-Z2 = (SIZE/2)+260;
+Z2 = (SIZE/2)+110;
 z2 = Z2*delx;
 Exz1 = zeros(maxTime,1); % record Electric field at 750
 Exz2 = zeros(maxTime,1); % record electric field at 760
 fspan = 100; % Points to plot in frequency domain
-
+%% Main Loop
 for medium= 1:2
     % Medium Specifications
     if medium==1
         mu=1.2566e-006*ones(1,SIZE);   %permeability of free sapce
         epsilon=8.8542e-012*ones(1,SIZE); % free space
-        epsilonr=1;
-        mur=1;
+        epsilonr=ones(1,SIZE);
+        mur=ones(1,SIZE);
     else
-        epsilon=[8.8542e-012*ones(1,SIZE-(SIZE/2)) -8.8542e-012*ones(1,(SIZE/2))]; % half medium
-        mu=[1.2566e-006*ones(1,SIZE-(SIZE/2)) -1.2566e-006*ones(1,(SIZE/2))];
-        epsilonr=-1;
-        mur=-1;
+        epsilon=[8.8542e-012*ones(1,SIZE-(SIZE/2)) -8.8542e-012*ones(1,(SIZE/4)) 8.8542e-012*ones(1,(SIZE/4))]; % half medium
+        mu=[1.2566e-006*ones(1,SIZE-(SIZE/2)) -1.2566e-006*ones(1,(SIZE/4)) 1.2566e-006*ones(1,(SIZE/4))];
+        epsilonr=[ones(1,SIZE-(SIZE/2)) -1*ones(1,SIZE/4) ones(1,SIZE/4)];
+        mur=[ones(1,SIZE-(SIZE/2)) -1*ones(1,SIZE/4) ones(1,SIZE/4)];
     end
-    mu_inf=1;
-    epsilon_inf=1;
-    omega_pe2= 4*pi*pi*f*f*(epsilon_inf-epsilonr);%Plasma frequency electric squared
-    omega_pm2= 4*pi*pi*f*f*(mu_inf-mur);%Plasma frequency magnetic squared
-    ro_m=0;
-    ro_e=0;
-    m_divide=((4*mu_nought*mu_inf)+(mu_nought*omega_pm2*(delt*delt))+(mu_nought*mu_inf*ro_m*(2*delt)));
-    am= 4/m_divide;
-    bm= (ro_m*2*delt)/m_divide;
-    cm= (4*mu_nought*mu_inf)/m_divide;
-    dm= (-mu_nought*omega_pm2*delt*delt)/m_divide;
-    em= (mu_nought*mu_inf*ro_m*2*delt)/m_divide;
-    e_divide=((4*epsilon_nought*epsilon_inf)+(epsilon_nought*omega_pe2*(delt*delt))+(epsilon_nought*epsilon_inf*ro_e*(2*delt)));
-    ae= 4/e_divide;
-    be= (ro_e*2*delt)/e_divide;
-    ce= (4*epsilon_nought*epsilon_inf)/e_divide;
-    de= (-epsilon_nought*omega_pe2*delt*delt)/e_divide;
-    ee= (epsilon_nought*epsilon_inf*ro_e*2*delt)/e_divide;
-
+    for  mm = 1:(SIZE)
+        omega_pe2= 4*pi*pi*f*f*(epsilon_inf-epsilonr(mm));%Plasma frequency electric squared
+        omega_pm2= 4*pi*pi*f*f*(mu_inf-mur(mm));%Plasma frequency magnetic squared
+        m_divide=((4*mu_nought*mu_inf)+(mu_nought*omega_pm2*(delt*delt))+(mu_nought*mu_inf*ro_m*(2*delt)));
+        am(mm)= 4/m_divide;
+        bm(mm)= (ro_m*2*delt)/m_divide;
+        cm(mm)= (4*mu_nought*mu_inf)/m_divide;
+        dm(mm)= (-mu_nought*omega_pm2*delt*delt)/m_divide;
+        em(mm)= (mu_nought*mu_inf*ro_m*2*delt)/m_divide;
+        e_divide=((4*epsilon_nought*epsilon_inf)+(epsilon_nought*omega_pe2*(delt*delt))+(epsilon_nought*epsilon_inf*ro_e*(2*delt)));
+        ae(mm)= 4/e_divide;
+        be(mm)= (ro_e*2*delt)/e_divide;
+        ce(mm)= (4*epsilon_nought*epsilon_inf)/e_divide;
+        de(mm)= (-epsilon_nought*omega_pe2*delt*delt)/e_divide;
+        ee(mm)= (epsilon_nought*epsilon_inf*ro_e*2*delt)/e_divide;
+    end
     % Temp Variable
     ez=zeros(1,SIZE);
     ezn_0=zeros(1,SIZE-1);
@@ -95,6 +99,10 @@ for medium= 1:2
     ezm1q=0;
     
     for qTime = 1:(maxTime-1)
+        % Drudes model Calculations
+        
+        
+        %storing pervious time steps values
         hyn_1=hyn_0;
         hyn_0=hy;
         byn_1=byn_0;
@@ -109,7 +117,7 @@ for medium= 1:2
         end
 %        Update Magnetic field
         for  mm = 1:(SIZE-1)  %changed it from 1 to 2 because of by(0) index problem at 1
-            hy(mm) = (am*(by(mm)-2*byn_0(mm)+byn_1(mm)))+(bm*(by(mm)-byn_1(mm)))+(cm*((2*hyn_0(mm))-(hyn_1(mm))))+(dm*((2*hyn_0(mm))+(hyn_1(mm))))+(em*(hyn_1(mm)));
+            hy(mm) = (am(mm)*(by(mm)-2*byn_0(mm)+byn_1(mm)))+(bm(mm)*(by(mm)-byn_1(mm)))+(cm(mm)*((2*hyn_0(mm))-(hyn_1(mm))))+(dm(mm)*((2*hyn_0(mm))+(hyn_1(mm))))+(em(mm)*(hyn_1(mm)));
         end
 %         Update Dz
         for mm = 2:(SIZE-1)
@@ -117,44 +125,39 @@ for medium= 1:2
         end
 %         Update Electrical filed
         for mm = 2:(SIZE-1)
-            ez(mm) = (ae*(dz(mm)-(2*dzn_0(mm))+dzn_1(mm)))+(be*(dz(mm)-dzn_1(mm)))+(ce*((2*ezn_0(mm))-ezn_1(mm)))+(de*((2*ezn_0(mm))+ezn_1(mm)))+(ee*ezn_1(mm));
+            ez(mm) = (ae(mm)*(dz(mm)-(2*dzn_0(mm))+dzn_1(mm)))+(be(mm)*(dz(mm)-dzn_1(mm)))+(ce(mm)*((2*ezn_0(mm))-ezn_1(mm)))+(de(mm)*((2*ezn_0(mm))+ezn_1(mm)))+(ee(mm)*ezn_1(mm));
         end
         Etemp(qTime)= ez(SIZE-(SIZE/2)+2); %just after boundary of medium
-        if SourceSelect==2
+        if SourceSelect==0
 %         Source node (hard coded)
 		    ez(2) = ez(2)+ (sin(2*pi*(qTime)*f*delt)*Sc);
          else
 		    ez(2) = ez(2)+exp(-(qTime - 30) * (qTime - 30) / (PulseWidth./4));
         end
 %         Absorbing Boundary Conditions
-        ez(1)=ez2q+(ez(2)-ez1q)*(((Sc/(mur*(epsilonr))^0.5)-1)/((Sc/(mur*(epsilonr))^0.5)+1));
-        ez(SIZE)=ezm1q+(ez(SIZE-1)-ezmq)*(((Sc/(mur*(epsilonr))^0.5)-1)/((Sc/(mur*(epsilonr))^0.5)+1));
-        dz(SIZE)=epsilonr*epsilon_nought; %epsilon nough
+        ez(1)=ez2q+(ez(2)-ez1q)*(((Sc/(mur(1)*(epsilonr(1)))^0.5)-1)/((Sc/(mur(1)*(epsilonr(1)))^0.5)+1));
+        ez(SIZE)=ezm1q+(ez(SIZE-1)-ezmq)*(((Sc/(mur(1)*(epsilonr(1)))^0.5)-1)/((Sc/(mur(1)*(epsilonr(1)))^0.5)+1));
+        dz(SIZE)=epsilonr(1)*epsilon_nought; %epsilon nough
 %         Saving q-1 (pervious step time values) for boundary Conditions
 		ez2q=ez(2);
 		ez1q=ez(1);
 		ezmq=ez(SIZE);
 		ezm1q=ez(SIZE-1);
 %         Plotting
-%        if medium==2
-%         figure(1);
+       if mod(qTime,10)==0
+       if medium==2
+        figure(1);
 %         subplot(2,1,1);
-%         plot(1:SIZE,ez);
-%         title('Electirc Component');
-%         xlim([0 SIZE]);
+        plot(1:SIZE,ez);
+        title('Electirc Component');
+        xlim([0 SIZE]);
 %         ylim([-1.2 1.2]);
 %         if medium==2
-%             line([SIZE-(SIZE/2) SIZE-(SIZE/2)],[-1.2 1.2],'Color','Red') % Medium slab line
+            line([SIZE-(SIZE/2) SIZE-(SIZE/2)],[-50 50],'Color','Red') % Medium slab line
+            line([SIZE-(SIZE/4) SIZE-(SIZE/4)],[-50 50],'Color','Red') % Medium slab line
 %         end
-%         subplot(2,1,2);
-%         plot(1:SIZE-1,hy);
-%         title('Magnetic Component');
-%         xlim([0 SIZE]);
-%         ylim([-0.005 0.005]);
-%         if medium==2
-%         line([SIZE-500 SIZE-500],[-0.005 0.005],'Color','Red') % Medium slab line
-%         end
-%        end
+       end
+       end
           Exz1(qTime)=ez(Z1);
           Exz2(qTime)=ez(Z2);
           
@@ -166,7 +169,7 @@ for medium= 1:2
         Etransmitted=Etemp;
     end
 end
-
+%% Post processing
 % Frequency Domain Analysis
 Fs=1/delt;   %Sampling Frequency
 T=1/Fs;      %Sample Time
@@ -220,7 +223,7 @@ plot(f(1:fspan), real(nFDTD(1:fspan)));
 title('Refractive index re(n)');
 xlabel('Frequency (Hz)');
 ylabel('re(n)');
-line([3e9 3e9],[-15 1.415],'Color','Red')
-line([0e9 3e9],[1.415 1.415],'Color','Red')
+line([3e9 3e9],[-15 -1],'Color','Red')
+line([0e9 3e9],[-1 -1],'Color','Red')
 
 ReferectiveIndex=(1/(k0*(Z2-Z1)*i))*log(FEtransmitted(Z2)/FEtransmitted(Z1))
